b952b868ee7dc8d5a5e4a22d3dbe28a900a5ab1f
Veeresh-Ranjan <99407299+Veeresh-Ranjan@users.noreply.github.com>
2022-04-25 18:10:38 +0530

    Add files via upload

--- misaligned.cpp
+++ misaligned.cpp
@@ -15,7 +15,8 @@ int printColorMap() {
 
 int main() {
     int result = printColorMap();
-    assert(result == 25);
+    assert(result == 0);
     std::cout << "All is well (maybe!)\n";
     return 0;
 }
+
